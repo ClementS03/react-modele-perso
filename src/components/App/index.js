a4bfbebaa373c67c025849d8208848b6869d8e05
ClementS03 <clement.seguin63@gmail.com>
2021-11-01 16:12:31 +0100

    install all dependencies and fix problems with eslint configuration

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <div>Header</div>
+    <div>Ingredients</div>
+    <div>Steps</div>
+  </div>
+);
+
+// == Export
+export default App;
